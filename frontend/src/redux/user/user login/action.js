b7203a524cf84291cd06782a9cef40d2e896ca33
Kunal Deotale <kdeotale4@gmail.com>
2023-05-09 21:04:02 +0530

    fix some bug

--- frontend/src/redux/user/user login/action.js
+++ frontend/src/redux/user/user login/action.js
@@ -4,7 +4,7 @@ import { LOGIN_FAILURE, LOGIN_REQUEST, LOGIN_SUCCESS } from "./actionType";
 export const loginData = (payload) => async (dispatch) => {
   dispatch({ type: LOGIN_REQUEST });
   try {
-    const res = await axios.post(`https://reqres.in/api/login`, payload);
+    const res = await axios.post(`https://clumsy-miniskirt-tuna.cyclic.app/users/login`, payload);
     dispatch({ type: LOGIN_SUCCESS, payload: res.data.token });
   } catch {
     dispatch({ type: LOGIN_FAILURE });
---
+++ frontend/src/redux/user/user signup/actionType.js
@@ -0,0 +1,4 @@
+export const REQUEST_LOADING = "request_loading";
+export const REQUEST_PENDING = "request_pending";
+export const POST_REQUEST_SUCCESS = "post_request_success";
+
