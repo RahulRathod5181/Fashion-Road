eeaf4e5c6822821c4c5d1dc313bfc6074038caa7
RahulRathod5181 <rahulrathod5181@gmail.com>
2023-05-10 04:10:34 +0530

    productPageReducer

--- frontend/src/redux/ProductPageReducer/action.js
+++ frontend/src/redux/ProductPageReducer/action.js
@@ -30,3 +30,31 @@ export const getProductMen = (query)=>(dispatch)=>{
         dispatch({type:GET_DATA_ERROR})
     })
 }
+export const getSingleMen = (id)=>(dispatch)=>{
+    // console.log(data)
+    // dispatch({type:GET_DATA,payload:data});
+    dispatch({type:GET_DATA_REQUEST})
+
+    axios.get(`https://clumsy-miniskirt-tuna.cyclic.app/products/mens/${id}`)
+    .then((res)=>{
+        // console.log(res.data)
+        dispatch({type:GET_DATA,payload:res.data})
+    })
+    .catch((err)=>{
+        dispatch({type:GET_DATA_ERROR})
+    })
+}
+export const getSingleWomen = (id)=>(dispatch)=>{
+    // console.log(data)
+    // dispatch({type:GET_DATA,payload:data});
+    dispatch({type:GET_DATA_REQUEST})
+
+   return axios.get(`https://clumsy-miniskirt-tuna.cyclic.app/products/womens/${id}`)
+    .then((res)=>{
+        // console.log(res.data)
+        dispatch({type:GET_DATA,payload:res.data})
+    })
+    .catch((err)=>{
+        dispatch({type:GET_DATA_ERROR})
+    })
+}
