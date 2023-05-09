40a25752603fbf8105d6c669ff9dbd24dfb256fa
Rahul Singh <rahul1905.sk@gmail.com>
2023-05-09 21:18:08 +0530

    checking

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import CartEmpty from "./component/Rahul/Cart/CartEmpty";
 import CartPage from "./component/Rahul/Cart/CartPage";
 import AddressForm from "./component/Rahul/PaymentPage/AddressForm";
 import CreditCard from "./component/Rahul/PaymentPage/CreditCard";
+import DebitCard from "./component/Rahul/PaymentPage/DebitCard";
+import OrderSucess from "./component/Rahul/PaymentPage/OrderSucess";
 import PaymentPage from "./component/Rahul/PaymentPage/PaymentPage";
 import { textTospeechFun } from "./component/Rahul/VoiceFun";
 import AllRoutes from "./component/utility/AllRoutes";
@@ -14,11 +16,12 @@ function App() {
     <div className="App">
       {/* <AllRoutes /> */}
       {/* <CartEmpty /> */}
-      {/* <CartPage /> */}
-{/* <PaymentPage /> */}
+      <CartPage />
+<PaymentPage />
 {/* <CreditCard /> */}
 {/* <AddressForm /> */}
-
+{/* <OrderSucess /> */}
+{/* <DebitCard /> */}
      </div>
   );
 }
