5d80147a9f31ba69f73c4ecb7152acf0296d714d
Kunal Deotale <kdeotale4@gmail.com>
2023-05-09 21:03:28 +0530

    fix some bug

--- frontend/src/redux/admin/admin product/action.js
+++ frontend/src/redux/admin/admin product/action.js
@@ -3,15 +3,29 @@ import {
   REQUEST_LOADING,
   REQUEST_PENDING,
   GET_REQUEST_SUCCESS,
+  ADD_REQUEST_SUCCESS,
 } from "./actionType";
 
 export const getData = (dispatch) => {
   dispatch({ type: REQUEST_LOADING });
 
   axios
-    .get(`https://project-backend-t6y7.onrender.com/products`)
+    .get(`https://clumsy-miniskirt-tuna.cyclic.app/admin`)
     .then((res) => {
       dispatch({ type: GET_REQUEST_SUCCESS, payload: res.data });
+    })
+    .catch((err) => {
+      dispatch({ type: REQUEST_PENDING });
+    });
+};
+
+export const addProduct = (payload) => (dispatch) => {
+  dispatch({ type: REQUEST_LOADING });
+
+  axios
+    .post(`https://project-backend-t6y7.onrender.com/products`, payload,{headers:{'Authorization':`${localStorage.getItem('adminToken')}`}})
+    .then(() => {
+      dispatch({ type: ADD_REQUEST_SUCCESS });
     })
     .catch((err) => {
       dispatch({ type: REQUEST_PENDING });
