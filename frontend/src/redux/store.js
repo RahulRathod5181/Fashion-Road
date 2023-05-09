9b0b47ceffb2a48e38829d60652502eda28b1850
Rahul Singh <rahul1905.sk@gmail.com>
2023-05-09 21:04:52 +0530

    store updated

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,11 +1,12 @@
 import { applyMiddleware, combineReducers, legacy_createStore } from "redux";
 import thunk from "redux-thunk";
 // import {reducer as ProductPageReducer} from "./ProductPageReducer/reducer"
-import {reducer as ProductPageReducer} from "./ProductPageRedux/reducer"
-import {reducer as cartPageReducer} from "./CartReducer/reducer"
+import {reducer as ProductPageReducer} from "./ProductPageRedux/reducer";
+import {reducer as cartPageReducer} from "./CartReducer/reducer";
 let rootReducer = combineReducers({
     ProductPageReducer,
     cartPageReducer
 });
+
 
 export const store = legacy_createStore(rootReducer, applyMiddleware(thunk));
