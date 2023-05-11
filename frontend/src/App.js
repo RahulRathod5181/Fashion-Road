fe937ccf15e92119e5b4edcce8ac955aab1a82be
Rahul Singh <rahul1905.sk@gmail.com>
2023-05-12 03:49:26 +0530

    app.js added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,17 @@
-import "./App.css";
-import CartEmpty from "./component/Rahul/Cart/CartEmpty";
-import CartPage from "./component/Rahul/Cart/CartPage";
-import AddressForm from "./component/Rahul/PaymentPage/AddressForm";
-import CreditCard from "./component/Rahul/PaymentPage/CreditCard";
-import DebitCard from "./component/Rahul/PaymentPage/DebitCard";
-import OrderSucess from "./component/Rahul/PaymentPage/OrderSucess";
-import PaymentPage from "./component/Rahul/PaymentPage/PaymentPage";
-import { textTospeechFun } from "./component/Rahul/VoiceFun";
+import "./App.css"; 
 import AllRoutes from "./component/utility/AllRoutes";
-import Footer from "./component/utility/Footer";
+
 
 function App() {
+ 
+  // textTospeechFun
   return (
     <div className="App">
   
-      <CartPage />
-<PaymentPage />
- 
- 
-     
+    
       <AllRoutes />
-      <Footer />
+ 
+
     </div> 
   );
 }
