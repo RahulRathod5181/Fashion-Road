0a59ded2a733256e6610842a109c69b65c82335c
Rahul Singh <rahul1905.sk@gmail.com>
2023-05-12 02:59:10 +0530

    code added

--- frontend/src/component/Rahul/PaymentPage/AddressForm.jsx
+++ frontend/src/component/Rahul/PaymentPage/AddressForm.jsx
@@ -1,78 +1,135 @@
-import { FormControl, FormErrorMessage, FormLabel, Input, InputGroup, InputLeftAddon, Radio, RadioGroup, Stack } from "@chakra-ui/react";
+import {
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputLeftAddon,
+  Radio,
+  RadioGroup,
+  Stack,
+} from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
 
 const AddressForm = () => {
-
-  const [formData, setFormData] = useState({pincode: '', mobileNo: '', name: ''})
-
+  const [formData, setFormData] = useState({
+    pincode: "",
+    // mobileNo: "",
+    name: "",
+  });
 
   useEffect(() => {
-  
-    localStorage.setItem('formData', JSON.stringify(formData))
-  }, [formData])
-  
+    localStorage.setItem("formData", JSON.stringify(formData));
+  }, [formData]);
 
-// console.log(formData)
+  // console.log(formData)
   return (
-    <Stack bgColor={'white'} borderRadius={'.35rem'} p={'10px 30px'} as="form" >
-      <Stack direction={'row'}>
-        <FormControl isInvalid={formData.pincode === '' ? true : false}  >
+    <Stack bgColor={"white"} borderRadius={".35rem"}
+    p={"10px 30px"}
+    pl={{ base: "0.3rem", sm: "0.3rem", md: "0.3rem", lg: "30px" }}
+    pr={{ base: "0.3rem", sm: "0.3rem", md: "0.3rem", lg: "30px" }}
+     as="form">
+      <Stack direction={"row"} margin={"auto"} w={'100%'}> 
+        <FormControl isInvalid={formData.pincode === "" ? true : false}>
           <FormLabel>Pincode</FormLabel>
-          <Input variant={'unstyled'} maxLength={6} value={formData.pincode} onChange={(e)=>setFormData({...formData, pincode: e.target.value})}  borderRadius={'none'} borderBottom={'1px solid grey'}   />
+          <Input
+            variant={"unstyled"}
+            maxLength={6}
+            value={formData.pincode}
+            onChange={(e) =>
+              setFormData({ ...formData, pincode: e.target.value })
+            }
+            borderRadius={"none"}
+            borderBottom={"1px solid grey"}
+          />
           <FormErrorMessage>Pincode is required.</FormErrorMessage>
         </FormControl>
-        <FormControl isInvalid={formData.mobileNo === '' ? true : false} >
+        <FormControl isInvalid={formData.mobileNo === "" ? true : false}>
           <FormLabel>Mobile Number </FormLabel>
 
           <InputGroup>
-    <InputLeftAddon outline={'none'} h={'auto'} border={'none'} bgColor={'white'} borderRadius={'none'} borderBottom={'1px solid grey'} p={'0'} m={0}>
-      +91
-    </InputLeftAddon>
-    <Input  maxLength={10} value={formData.mobileNo} onChange={(e)=>setFormData({...formData, mobileNo: e.target.value})}   type='tel'  variant={'unstyled'}  borderRadius={'none'}  borderBottom={'1px solid grey'} />
-    <FormErrorMessage>Mobile Number is required.</FormErrorMessage> 
-  </InputGroup>
+            <InputLeftAddon
+              outline={"none"}
+              h={"auto"}
+              border={"none"}
+              bgColor={"white"}
+              borderRadius={"none"}
+              borderBottom={"1px solid grey"}
+              p={"0"}
+              m={0}
+            >
+              +91
+            </InputLeftAddon>
+            <Input
+              maxLength={10}
+              value={formData.mobileNo}
+          
+              onChange={(e) =>
+                setFormData({ ...formData, mobileNo: e.target.value })
+              }
+              type="tel"
+              variant={"unstyled"}
+              borderRadius={"none"}
+              borderBottom={"1px solid grey"}
+            // border = {'2px solid red'}
+            />
 
-
-         
+            {/* <FormErrorMessage> Mobile Number is required.</FormErrorMessage> */}
+          </InputGroup>
         </FormControl>
       </Stack>
-      <FormControl isInvalid={formData.name === '' ? true : false} >
+      <FormControl isInvalid={formData.name === "" ? true : false}>
         <FormLabel>Full Name </FormLabel>
-        
-        <Input  value={formData.name} onChange={(e)=>setFormData({...formData, name: e.target.value})}   variant={'unstyled'}  borderRadius={'none'} borderBottom={'1px solid grey'} />
+
+        <Input
+          value={formData.name}
+          onChange={(e) => setFormData({ ...formData, name: e.target.value })}
+          variant={"unstyled"}
+          borderRadius={"none"}
+          borderBottom={"1px solid grey"}
+        />
         <FormErrorMessage>Full Name is required.</FormErrorMessage>
       </FormControl>
       <FormControl isRequired>
-        <FormLabel>
-Flat / House No. / Building Name</FormLabel>
+        <FormLabel>Flat / House No. / Building Name</FormLabel>
 
-<Input   variant={'unstyled'}  borderRadius={'none'} borderBottom={'1px solid grey'}   />
+        <Input
+          variant={"unstyled"}
+          borderRadius={"none"}
+          borderBottom={"1px solid grey"}
+        />
       </FormControl>
-  
-      <Stack direction={'row'} >
+
+      <Stack direction={"row"}>
         <FormControl isRequired>
           <FormLabel>City</FormLabel>
-          
-          <Input   variant={'unstyled'}  borderRadius={'none'} borderBottom={'1px solid grey'}  />
+
+          <Input
+            variant={"unstyled"}
+            borderRadius={"none"}
+            borderBottom={"1px solid grey"}
+          />
         </FormControl>
         <FormControl isRequired>
           <FormLabel>State </FormLabel>
-          
-          <Input   variant={'unstyled'}  borderRadius={'none'} borderBottom={'1px solid grey'}  />
+
+          <Input
+            variant={"unstyled"}
+            borderRadius={"none"}
+            borderBottom={"1px solid grey"}
+          />
         </FormControl>
       </Stack>
 
       <FormControl>
-          <FormLabel>Address Type</FormLabel>
-          <RadioGroup defaultValue='addressType'>
-    <Stack direction={'row'} justify={'flex-start'} spacing={'150px'}>
-      <Radio  value='home'>Home</Radio>
-      <Radio value='office'>Office</Radio>
-    
-    </Stack>
-  </RadioGroup>
-         </FormControl>
-
+        <FormLabel>Address Type</FormLabel>
+        <RadioGroup defaultValue="addressType">
+          <Stack direction={"row"} justify={"flex-start"} spacing={"150px"}>
+            <Radio value="home">Home</Radio>
+            <Radio value="office">Office</Radio>
+          </Stack>
+        </RadioGroup>
+      </FormControl>
     </Stack>
   );
 };
--- frontend/src/component/Rahul/VoiceFun.js
+++ frontend/src/component/Rahul/VoiceFun.js
@@ -1,11 +1,36 @@
 
 
-let voices = window.speechSynthesis.getVoices();
-
 
 export const textTospeechFun = (text) => {
+    let voices = window.speechSynthesis.getVoices()[3];
+    console.log(voices);
+
+    console.log('voiceFun working', text);
      const utternance =  new SpeechSynthesisUtterance(text);
-    utternance.voice =  window.speechSynthesis.getVoices()[3] ;
-    speechSynthesis.speak(utternance);
+     if(window.speechSynthesis.getVoices()[3] === null) {
+        console.log('empty gfbkjhnhfj');
+     }
+    utternance.voice =  voices ;
+    window.speechSynthesis.speak(utternance);
+      
+
 }
 
+
+// export const textTospeechFun =(text) => {
+//     console.log('text working');
+//     // Check if the browser supports the SpeechSynthesis API
+//     if ('speechSynthesis' in window) {
+//       // Create a new SpeechSynthesisUtterance instance
+//       const message = new SpeechSynthesisUtterance(text);
+  
+//       // Use the default voice
+//       message.voice = speechSynthesis.getVoices()[0];
+  
+//     //   Speak the text
+//       speechSynthesis.speak(message);
+//     } else {
+//       console.log('Speech synthesis is not supported.');
+//     }
+//   }
+  
